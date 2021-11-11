e99d167877b38a0d363bc9e124141b9b0dda24e7
Deyvid <>
2021-11-11 06:57:52 +0000

    02.00

--- src/exercise5/UniqueNumbers.java
+++ src/exercise5/UniqueNumbers.java
@@ -20,7 +20,43 @@ public class UniqueNumbers {
         int[] uniqueNumbers = new int[numbers.length];
         // Find unique numbers in numbers
 
+        bubbleSort(numbers);
+
+        int j = 0;
+        int br = 0;
+        int counter = 0;
+        for(int i = 1; i < numbers.length; i++) {
+          if(numbers[j] != numbers[i]) {
+            uniqueNumbers[br] = numbers[j];
+            j = i;
+            br++;
+            counter = 0;
+          } 
+          counter++;
+          if(i == (numbers.length - 1) && counter != 0) {
+            uniqueNumbers[br] = numbers[j];
+          }
+        }
+
         String uniqueNumbersAsString = Arrays.toString(uniqueNumbers);
         System.out.println("Unique numbers: " + uniqueNumbersAsString);
     }
+
+    static void bubbleSort(int[] array) {
+            boolean swapped = true;
+            int j = 0;
+            int tmp;
+            while (swapped) {
+                swapped = false;
+                j++;
+                for (int i = 0; i < array.length - j; i++) {
+                    if (array[i] > array[i + 1]) {
+                        tmp = array[i];
+                        array[i] = array[i + 1];
+                        array[i + 1] = tmp;
+                        swapped = true;
+                    }
+                }
+            }
+        }
 }
